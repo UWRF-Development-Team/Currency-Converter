05a0059c2a088c4da3d4133658916af5b49d8de1
UWRFDevTeam <uwrfacmdevteam@uwrf.edu>
2024-07-02 18:50:56 -0500

    Add supported currencies and their initialization in SupportedCurrencies.java

--- src/main/java/org/falcon/model/SupportedCurrencies.java
+++ src/main/java/org/falcon/model/SupportedCurrencies.java
@@ -1,4 +1,25 @@
 package org.falcon.model;
 
+import org.falcon.entity.currency.Currency;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SupportedCurrencies {
+    public static final Currency USD = new Currency("United States Dollar", "USD");
+    public static final Currency EUR = new Currency("Euro", "EUR");
+    public static final Currency CHF = new Currency("Swiss Franc", "CHF");
+    public static final Currency MXN = new Currency("Mexican Peso", "MXN");
+    public static final Currency RUB = new Currency("Russian Ruble", "RUB");
+    public static final Currency GBP = new Currency("British Pound", "GBP");
+    public static final List<Currency> SUPPORTED_CURRENCIES;
+    static {
+        SUPPORTED_CURRENCIES = new ArrayList<>();
+        SUPPORTED_CURRENCIES.add(USD);
+        SUPPORTED_CURRENCIES.add(EUR);
+        SUPPORTED_CURRENCIES.add(CHF);
+        SUPPORTED_CURRENCIES.add(MXN);
+        SUPPORTED_CURRENCIES.add(RUB);
+        SUPPORTED_CURRENCIES.add(GBP);
+    }
 }
