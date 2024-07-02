b80d56b9c64f40d27fa48cf8c5a4972e183900b3
UWRFDevTeam <uwrfacmdevteam@uwrf.edu>
2024-07-02 18:47:59 -0500

    Add entity properties and methods to Currency.java for managing currency data

--- src/main/java/org/falcon/entity/currency/Currency.java
+++ src/main/java/org/falcon/entity/currency/Currency.java
@@ -1,4 +1,43 @@
 package org.falcon.entity.currency;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.falcon.model.api.ApiConnection;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "currencies")
 public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String code;
+    private double value;
+    private LocalDateTime lastUpdated;
+    public Currency() {
+
+    }
+    public Currency(String name, String code) {
+        this.name = name;
+        this.code = code;
+        this.value = 0;
+        this.lastUpdated = LocalDateTime.now();
+        this.updateValueFromApi();
+    }
+    public Currency(String name, String code, double value) {
+        this.name = name;
+        this.code = code;
+        this.value = value;
+        this.lastUpdated = LocalDateTime.now();
+    }
+    public void updateValueFromApi() {
+        ApiConnection apiConnection = new ApiConnection();
+        apiConnection.fetchResponse();
+        this.lastUpdated = LocalDateTime.now();
+    }
 }
